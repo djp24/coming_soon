f58f38861abbf83e20831851abd6855d8b2a24c0
Dara Palmer <djpalmer1224@gmail.com>
2023-05-01 12:28:44 -0600

    Temporary SVG fix

--- js/main.js
+++ js/main.js
@@ -1,3 +1,38 @@
+/* Light & Dark Mode Logos 
+
+var userPrefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+var toggleDark = document.getElementById('toggle-dark');
+var toggleLight = document.getElementById('toggle-light');
+var htmlElem = document.querySelector('html');
+
+if (userPrefersDark.matches)) {
+  htmlElem.classList.add('dark');
+  toggleDark.classList.add('visible');
+  toggleLight.classList.remove('hidden');
+} else {
+  toggleLight.classList.add('visible');
+  toggleDark.classList.remove('hidden');
+}
+
+toggleLight.addEventListener('click', function () {
+    localStorage.setItem('theme', 'light');
+    htmlElem.classList.remove('dark');
+    toggleDark.classList.add('visible');
+    toggleDark.classList.remove('hidden');
+    toggleLight.classList.add('hidden');
+    toggleLight.classList.remove('visible');
+  });
+  
+  toggleDark.addEventListener('click', function () {
+    localStorage.setItem('theme', 'dark');
+    htmlElem.classList.add('dark');
+    toggleLight.classList.add('visible');
+    toggleLight.classList.remove('hidden');
+    toggleDark.classList.add('hidden');
+    toggleDark.classList.remove('visible');
+  });
+*/
+
 /* Light & Dark Mode Toggle  
 
 const darkIcon = document.querySelector('.dark');
